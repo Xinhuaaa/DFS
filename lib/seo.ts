b8131f3e1d326c6db8f9dc9cf840182d3c9a9bba
Xie BianQing <xinkerrr@gmail.com>
2024-10-03 22:46:53 +0800

    更新标题

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,5 +1,5 @@
 export const seo = {
-  title: 'Cali Castle | 开发者、摄影师、细节控、技术党',
+  title: 'DFStudio | 开发者、摄影师、细节控、技术党',
   description:
     '我是DFStudio，一个充满灵感与活力的创作团队。我们致力于打造激发创意的氛围，鼓励团队成员捕捉动人瞬间 ，创作触动人心的影像作品。我们热爱摄影、艺术、创新、生活以及在光影世界中不断探索。',
   url: new URL(
